167d1d3bba65af84bb73d2a48fbb6929fcc6f58c
Nathan Loding <nathan@loding.dev>
2026-01-07 17:08:41 -0500

    basic player movement implemented

---
+++ src/components/game-object/base-game-object.ts
@@ -0,0 +1,24 @@
+import { GameObject } from "../../common/types";
+
+export class BaseGameObjectComponent {
+  protected scene: Phaser.Scene;
+  protected gameObject: GameObject;
+
+  constructor(gameObject: GameObject) {
+    this.scene = gameObject.scene;
+    this.gameObject = gameObject;
+    this.assignComponentToObject(gameObject);
+  }
+
+  static getComponent<T>(gameObject: GameObject): T {
+    return gameObject[`_${this.name}`];
+  }
+
+  static removeComponent(gameObject: GameObject): void {
+    delete gameObject[`_${this.name}`];
+  }
+
+  protected assignComponentToObject(object: GameObject): void {
+    object[`_${this.constructor.name}`] = this;
+  }
+}
--- src/game-objects/player/player.ts
+++ src/game-objects/player/player.ts
@@ -2,6 +2,8 @@ import * as Phaser from 'phaser';
 import { Position } from "../../common/types";
 import { PLAYER_ANIMATION_KEYS } from '../../common/assets';
 import { InputComponent } from '../../components/input/input-component';
+import { ControlsComponent } from '../../components/game-object/controls-component';
+import { isArcadePhysicsBody } from '../../common/utils';
 
 export type PlayerConfig = {
   scene: Phaser.Scene;
@@ -12,7 +14,7 @@ export type PlayerConfig = {
 };
 
 export class Player extends Phaser.Physics.Arcade.Sprite {
-  #controls: InputComponent;
+  #controlsComponent: ControlsComponent;
 
   constructor(config: PlayerConfig) {
     const { scene, position, assetKey, frame } = config;
@@ -23,7 +25,7 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
     scene.add.existing(this);
     scene.physics.add.existing(this);
 
-    this.#controls = config.controls;
+    this.#controlsComponent = new ControlsComponent(this, config.controls);
 
     this.play({ key: PLAYER_ANIMATION_KEYS.IDLE_DOWN, repeat: -1 });
 
@@ -34,18 +36,62 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
   }
 
   update(): void {
-    if (this.#controls.isUpButtonDown) {
-      this.play({ key: PLAYER_ANIMATION_KEYS.IDLE_UP, repeat: -1 }, true);
-    } else if (this.#controls.isDownButtonDown) {
+    const controls = this.#controlsComponent.controls;
+
+    if (controls.isUpButtonDown) {
+      this.play({ key: PLAYER_ANIMATION_KEYS.WALK_UP, repeat: -1 }, true);
+      this.#updateVelocity(false, -1);
+    } else if (controls.isDownButtonDown) {
+      this.play({ key: PLAYER_ANIMATION_KEYS.WALK_DOWN, repeat: -1 }, true);
+      this.#updateVelocity(false, 1);
+    } else {
+      this.#updateVelocity(false, 0);
+    }
+
+    const isMovingVertically = controls.isDownButtonDown || controls.isUpButtonDown;
+    if (controls.isLeftButtonDown) {
+      this.setFlipX(true);
+      this.#updateVelocity(true, -1);
+      if (!isMovingVertically) {
+        this.play({ key: PLAYER_ANIMATION_KEYS.WALK_SIDE, repeat: -1 }, true);
+      }
+    } else if (controls.isRightButtonDown) {
+      this.setFlipX(false);
+      this.#updateVelocity(true, 1);
+      if (!isMovingVertically) {
+        this.play({ key: PLAYER_ANIMATION_KEYS.WALK_SIDE, repeat: -1 }, true);
+      }
+    } else {
+      this.#updateVelocity(true, 0)
+    }
+
+    if (!controls.isDownButtonDown && !controls.isUpButtonDown && !controls.isLeftButtonDown && !controls.isRightButtonDown) {
       this.play({ key: PLAYER_ANIMATION_KEYS.IDLE_DOWN, repeat: -1 }, true);
     }
 
-    if (this.#controls.isLeftButtonDown) {
-      this.setFlipX(true);
-      this.play({ key: PLAYER_ANIMATION_KEYS.IDLE_SIDE, repeat: -1 }, true);
-    } else if (this.#controls.isRightButtonDown) {
-      this.setFlipX(false);
-      this.play({ key: PLAYER_ANIMATION_KEYS.IDLE_SIDE, repeat: -1 }, true);
+    this.#normalizeVelocity();
+  }
+
+  #updateVelocity(isX: boolean, value: number): void {
+    if (!isArcadePhysicsBody(this.body)) {
+      return;
     }
+
+    this.body.velocity[`${isX ? 'x' : 'y'}`] = value;
+
+    // if (isX) {
+    //   this.body.velocity.x = value;
+    //   return;
+    // }
+
+    // this.body.velocity.y = value;
+  }
+
+  #normalizeVelocity(): void {
+    if (!isArcadePhysicsBody(this.body)) {
+      return;
+    }
+
+    this.body.velocity.normalize().scale(80);
   }
 }
